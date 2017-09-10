93fa8ff7bc8371fd313d193518d6a7a35695b675
Aryel Mota Góis <aryel.gois@gmail.com>
2017-09-10 09:26:11 -0300

    Undo edits in shipping_file example
    They were for testing

--- example/shipping_file.php
+++ example/shipping_file.php
@@ -9,13 +9,13 @@ $controller = new BankI\Controllers\ShippingFile($db_address, $db_banki, $config
 
 //output result
 if ($controller->execute()) {
-    //$filename = $controller->saveFile(__DIR__ . '/data/shipping_files');
-    //if ($filename != false) {
-        //echo '<h2>' . $filename . "</h2>\n";
+    $filename = $controller->saveFile(__DIR__ . '/data/shipping_files');
+    if ($filename != false) {
+        echo '<h2>' . $filename . "</h2>\n";
         echo '<pre>' . $controller->result . "</pre>\n\n\n";
-    //} else {
-        //echo '<p>Error saving file. Remember to give write permission to apache at data/*</p>';
-    //}
+    } else {
+        echo '<p>Error saving file. Remember to give write permission to apache at data/*</p>';
+    }
 } else {
     echo '<p>There are no titles to send</p>';
 }
