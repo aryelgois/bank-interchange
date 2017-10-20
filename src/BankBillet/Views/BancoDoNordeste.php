5cc080be2bc19e66d012e2054e301f02e206172e
Aryel Mota Góis <aryel.gois@gmail.com>
2017-10-19 23:02:21 -0300

    Update BancoDoNordeste
    I'm trying to homologate with this Bank, these seem
    to be their requirements

--- src/BankBillet/Views/BancoDoNordeste.php
+++ src/BankBillet/Views/BancoDoNordeste.php
@@ -17,4 +17,28 @@ use aryelgois\BankInterchange as BankI;
  * @link https://www.github.com/aryelgois/bank-interchange
  */
 class BancoDoNordeste extends namespace\CaixaEconomicaFederal
-{}
+{
+    /**
+     * Length used to zero-pad "Our Number"
+     */
+    const ONUM_LEN = 7;
+
+    /**
+     * Length used to zero-pad the account
+     */
+    const ACCOUNT_LEN = 7;
+
+    /**
+     * Free space, defined by Bank.
+     *
+     * Here: Agency/Assignor's code . Our number . Wallet operation . '000'
+     */
+    protected function generateFreeSpace()
+    {
+        $result = $this->formatAgencyCode(false)
+                . $this->formatOnum(false)
+                . '21'
+                . '000';
+        return $result;
+    }
+}
