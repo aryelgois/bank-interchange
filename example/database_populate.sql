fbaa6ad6f9c0c758e97dd3e5273b8d8fa7362749
Aryel Mota Góis <aryel.gois@gmail.com>
2017-11-09 23:51:14 -0300

    Update example database_populate

--- example/database_populate.sql
+++ example/database_populate.sql
@@ -1,15 +1,17 @@
+-- Run after root/data/{database.sql, database_populate.sql}
+
 USE bank_interchange;
 
-INSERT INTO `fulladdress` (`id`, `county`, `neighborhood`, `place`, `number`, `zipcode`, `detail`) VALUES
+INSERT INTO `full_addresses` (`id`, `county`, `neighborhood`, `place`, `number`, `zipcode`, `detail`) VALUES
 (1, 5389, 'PHP', 'Av. Universo', '42', '49000000', ''),
-(2, 5389, 'PHP', 'R. dos Developers', '101', '49000000', '');
+(2, 5389, 'PHP', 'R. dos Developers', '101', '49000000', 'Cond. Composer');
 
-INSERT INTO `banks` (`id`, `code`, `name`, `view`, `logo`, `tax`) VALUES
-(1, '104', 'Caixa Econômica Federal', 'CaixaEconomicaFederal', 'caixa.jpg', '2.0000'),
-(1, '047', 'Banese', 'Banese', 'banese.jpg', '2.5000');
+INSERT INTO `people` (`id`, `name`, `document`) VALUES
+(1, 'My e-Comerce', '00000000000006'),
+(2, 'Nome do Cliente', '00000000191');
 
-INSERT INTO `assignors` (`id`, `bank`, `address`, `document`, `name`, `covenant`, `agency`, `agency_cd`, `account`, `account_cd`, `edi7`, `logo`, `url`) VALUES
-(1, 1, 1, '00000000000006', 'My e-Comerce', '111', '222', '3', '4444444', '5', '666666', 'example.png', 'www.example.com');
+INSERT INTO `assignors` (`id`, `person`, `address`, `bank`, `wallet`, `covenant`, `agency`, `agency_cd`, `account`, `account_cd`, `edi`, `logo`, `url`) VALUES
+(1, 1, 1, 1, 2, '111', '222', '3', '4444444', '5', '666666', 'example.png', 'www.example.com');
 
-INSERT INTO `payers` (`id`, `address`, `document`, `name`) VALUES
-(1, 1, '00000000191', 'Nome do Cliente');
+INSERT INTO `payers` (`id`, `person`, `address`) VALUES
+(1, 2, 2);
