86ee924d737814e9b6e6cb704c5684a7e3276478
Aryel Mota Góis <aryel.gois@gmail.com>
2017-11-10 14:49:42 -0300

    Update Database

--- data/database.sql
+++ data/database.sql
@@ -37,10 +37,10 @@ CREATE TABLE `species` (
 
 CREATE TABLE `wallets` (
     `id`            tinyint(3)      UNSIGNED NOT NULL AUTO_INCREMENT,
-    `febraban`      tinyint(2)      NOT NULL,
-    `operation`     tinyint(2)      NOT NULL,
     `symbol`        char(2)         NOT NULL,
     `name`          varchar(60)     NOT NULL,
+    `febraban`      tinyint(2)      NOT NULL,
+    `operation`     tinyint(2)      NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -96,7 +96,6 @@ CREATE TABLE `titles` (
     `specie`        tinyint(3)      UNSIGNED NOT NULL,
     `our_number`    int(10)         UNSIGNED NOT NULL,
     `status`        tinyint(1)      UNSIGNED NOT NULL DEFAULT 0,
-    `cnab`          char(3)         NOT NULL,
     `doc_type`      char(1)         NOT NULL DEFAULT 1,
     `kind`          tinyint(2)      UNSIGNED NOT NULL,
     `value`         decimal(17,4)   NOT NULL,
