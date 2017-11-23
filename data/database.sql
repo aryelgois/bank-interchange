2e1bff02f8bcbcaf5dc33e9233a00117b76137ee
Aryel Mota Góis <aryel.gois@gmail.com>
2017-11-23 19:48:56 -0300

    Update Database
    Status are nullable

--- data/database.sql
+++ data/database.sql
@@ -96,7 +96,7 @@ CREATE TABLE `titles` (
     `guarantor`     int(10)         UNSIGNED,
     `specie`        tinyint(3)      UNSIGNED NOT NULL,
     `our_number`    int(10)         UNSIGNED NOT NULL,
-    `status`        tinyint(1)      UNSIGNED NOT NULL DEFAULT 0,
+    `status`        tinyint(1),
     `doc_type`      char(1)         NOT NULL DEFAULT 1,
     `kind`          tinyint(2)      UNSIGNED NOT NULL,
     `value`         decimal(17,4)   NOT NULL,
@@ -123,7 +123,7 @@ CREATE TABLE `titles` (
 CREATE TABLE `shipping_files` (
     `id`            int(10)         UNSIGNED NOT NULL AUTO_INCREMENT,
     `assignor`      int(10)         UNSIGNED NOT NULL,
-    `status`        tinyint(1)      UNSIGNED NOT NULL DEFAULT 0,
+    `status`        tinyint(1),
     `stamp`         timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update`        datetime,
     PRIMARY KEY (`id`),
