8fc33d8b2ceb3844a78d597d88b3522b135c805a
Aryel Mota Góis <aryel.gois@gmail.com>
2017-11-23 23:54:05 -0300

    Update example process_return_file

--- example/process_return_file.php
+++ example/process_return_file.php
@@ -6,4 +6,44 @@ use aryelgois\BankInterchange;
 
 $return_file = BankInterchange\Controllers\ReturnFile::process($_POST['return_file']);
 
-var_dump($return_file);
+$messages = $return_file->getMessages();
+
+$info = "<ul>\n";
+foreach ($messages['info'] as $m) {
+    $text = implode("<br />\n", array_filter([
+        'Our Number: ' . $m['our_number'],
+        (isset($m['movement']) ? 'Movement: ' . $m['movement'] : ''),
+        'Occurrence: ' . $m['occurrence'],
+        (isset($m['occurrence_date']) ? 'Occurrence date: ' . $m['occurrence_date'] : ''),
+    ]));
+    $info .= '<li><p>' . $text . "</p></li>\n";
+}
+$info .= "</ul>\n\n";
+
+$count = count($messages['error']);
+$error = '<strong>' . $count . ' errors' . ($count > 0 ? ':' : '') . "</strong>\n<ul>\n";
+if ($count) {
+    $error .= '<li>' . implode("</li>\n<li>", $messages['error']) . "</li>\n";
+}
+$error .= "</ul>\n\n";
+
+$count = count($messages['warning']);
+$warning = '<strong>' . $count . ' warnings' . ($count > 0 ? ':' : '') . "</strong>\n<ul>\n";
+if ($count) {
+    $warning .= '<li>' . implode("</li>\n<li>", $messages['warning']) . "</li>\n";
+}
+$warning .= "</ul>\n\n";
+
+echo "<h2>Result:</h2>\n\n" . $info . $error . $warning;
+
+if (isset($_POST['apply'])) {
+    echo "<h2>Applying...</h2>\n";
+    $result = $return_file->apply();
+    if ($result === false) {
+        echo 'Nothing to apply';
+    } elseif ($result === true) {
+        echo 'Applied successfully';
+    } else {
+        echo 'Titles which failed: ' . implode(', ', $result);
+    }
+}
