24495839ba007a3d502a4807be8b333032406df6
Aryel Mota Góis <aryel.gois@gmail.com>
2017-11-25 08:51:16 -0300

    Fix indentation

--- example/index.php
+++ example/index.php
@@ -175,140 +175,140 @@ function select_all(source, name) {
     </aside>
 
     <main>
-    <section id="intro">
-        <h2>Intro</h2>
-        <p>
-            This example represents a fictional e-Comerce
-        </p>
-    </section>
+        <section id="intro">
+            <h2>Intro</h2>
+            <p>
+                This example represents a fictional e-Comerce
+            </p>
+        </section>
 
-    <section id="setup">
-        <h2>Setup</h2>
-        <ol>
-            <li>
-                Create <a href="../data/database.sql">this database</a> in your
-                server, then populate it with <a href="../data/database_populate.sql">this</a>
-                and <a href="database_populate_example.sql">this</a>.
-            </li>
-            <li>
-                Configure the database options in <code>../config/medools.php</code>
-            </li>
-        </ol>
-    </section>
+        <section id="setup">
+            <h2>Setup</h2>
+            <ol>
+                <li>
+                    Create <a href="../data/database.sql">this database</a> in your
+                    server, then populate it with <a href="../data/database_populate.sql">this</a>
+                    and <a href="database_populate_example.sql">this</a>.
+                </li>
+                <li>
+                    Configure the database options in <code>../config/medools.php</code>
+                </li>
+            </ol>
+        </section>
 
-    <section id="generate_title">
-        <h2>Generate Title</h2>
-        <p>
-            It represents the client interaction in the Website.
-        </p>
-        <p>
-            The client would log in, choose some products (the value below is the
-            sum) and the server would known from who.
-        </p>
-        <form action="generate_title.php" method="POST">
-            <table>
-                <tr>
-                    <td>The client</td>
-                    <td>
-                        <select name="payer" required>
+        <section id="generate_title">
+            <h2>Generate Title</h2>
+            <p>
+                It represents the client interaction in the Website.
+            </p>
+            <p>
+                The client would log in, choose some products (the value below is the
+                sum) and the server would known from who.
+            </p>
+            <form action="generate_title.php" method="POST">
+                <table>
+                    <tr>
+                        <td>The client</td>
+                        <td>
+                            <select name="payer" required>
 <?php
 
 protected_example('list_payers');
 
 ?>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>is buying from</td>
-                    <td>
-                        <select name="assignor" required>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>is buying from</td>
+                        <td>
+                            <select name="assignor" required>
 <?php
 
 protected_example('list_assignors');
 
 ?>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>something of <span>R$</span></td>
-                    <td><input name="value" type="number" min="0.5" step="0.01" value="5" required /></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><button name="action" value="generate_title">Ok</button></td>
-                </tr>
-            </table>
-        </form>
-    </section>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>something of <span>R$</span></td>
+                        <td><input name="value" type="number" min="0.5" step="0.01" value="5" required /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button name="action" value="generate_title">Ok</button></td>
+                    </tr>
+                </table>
+            </form>
+        </section>
 
-    <section id="generate_shipping_file">
-        <h2>Generate Shipping File</h2>
-        <p>
-            Below is a list of all titles in the database. Those not yet in a
-            shipping file have a checkbox.
-        </p>
-        <form method="POST">
-            <table class="table-list">
-                <tr>
-                    <th><input type="checkbox" onchange="select_all(this, 'titles[]')" /></th>
-                    <th>id</th>
-                    <th>Client</th>
-                    <th>Assignor</th>
-                    <th>Value</th>
-                    <th>Date</th>
-                    <th>Billet</th>
-                </tr>
+        <section id="generate_shipping_file">
+            <h2>Generate Shipping File</h2>
+            <p>
+                Below is a list of all titles in the database. Those not yet in a
+                shipping file have a checkbox.
+            </p>
+            <form method="POST">
+                <table class="table-list">
+                    <tr>
+                        <th><input type="checkbox" onchange="select_all(this, 'titles[]')" /></th>
+                        <th>id</th>
+                        <th>Client</th>
+                        <th>Assignor</th>
+                        <th>Value</th>
+                        <th>Date</th>
+                        <th>Billet</th>
+                    </tr>
 <?php
 
 protected_example('list_titles');
 
 ?>
-            </table>
-            <button formaction="generate_shipping_file.php">Ok</button>
-            <p>
-                Remember that, in production, you have to generate and send the
-                Shipping File before outputing the Billet.
-            </p>
-        </form>
-    </section>
+                </table>
+                <button formaction="generate_shipping_file.php">Ok</button>
+                <p>
+                    Remember that, in production, you have to generate and send the
+                    Shipping File before outputing the Billet.
+                </p>
+            </form>
+        </section>
 
-    <section id="generate_cnab">
-        <h2>Generate CNAB</h2>
-        <p>
-            Below is a list of all Shipping Files in the database. Choose how you
-            want to render them.
-        </p>
-        <table class="table-list">
-            <tr>
-                <th>id</th>
-                <th>Titles</th>
-                <th>Total</th>
-                <th>Date</th>
-                <th>CNAB</th>
-            </tr>
+        <section id="generate_cnab">
+            <h2>Generate CNAB</h2>
+            <p>
+                Below is a list of all Shipping Files in the database. Choose how you
+                want to render them.
+            </p>
+            <table class="table-list">
+                <tr>
+                    <th>id</th>
+                    <th>Titles</th>
+                    <th>Total</th>
+                    <th>Date</th>
+                    <th>CNAB</th>
+                </tr>
 <?php
 
 protected_example('list_shipping_files');
 
 ?>
-        </table>
-    </section>
+            </table>
+        </section>
 
-    <section id="process_return_file">
-        <h2>Return File</h2>
-        <p>
-            Enter a Return File sent by a Bank to process it
-        </p>
-        <form action="process_return_file.php" method="POST">
-            <textarea name="return_file" required></textarea>
+        <section id="process_return_file">
+            <h2>Return File</h2>
             <p>
-                <label><input name="apply" type="checkbox" />Apply in the Database</label>
+                Enter a Return File sent by a Bank to process it
             </p>
-            <button>Send</button>
-        </form>
-    </section>
+            <form action="process_return_file.php" method="POST">
+                <textarea name="return_file" required></textarea>
+                <p>
+                    <label><input name="apply" type="checkbox" />Apply in the Database</label>
+                </p>
+                <button>Send</button>
+            </form>
+        </section>
     </main>
 </body>
 </html>
