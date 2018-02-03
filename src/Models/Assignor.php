aryelgois/bank-interchange

commit 81eff51a323a0501429f028c6b2f45516c98d202

Fix foreign model classes

src/Models/Assignor.php
@@ -36,11 +36,11 @@ class Assignor extends Medools\Model
 
     const FOREIGN_KEYS = [
         'person' => [
-            '\aryelgois\Medools\Models\Person',
+            'aryelgois\Medools\Models\Person',
             'id'
         ],
         'address' => [
-            __NAMESPACE__ . '\FullAddress',
+            'aryelgois\Databases\Models\Address\FullAddress',
             'id'
         ],
     ];

src/Models/Client.php
@@ -30,11 +30,11 @@ class Client extends Medools\Model
 
     const FOREIGN_KEYS = [
         'person' => [
-            '\aryelgois\Medools\Models\Person',
+            'aryelgois\Medools\Models\Person',
             'id'
         ],
         'address' => [
-            __NAMESPACE__ . '\FullAddress',
+            'aryelgois\Databases\Models\Address\FullAddress',
             'id'
         ],
         'assignor' => [

commit 9e851baf2f9b43b1983eea51bab4ae6ea6b80d2a

Fix foreign to FullAddress
Problem introduced in 81eff51a323a0501429f028c6b2f45516c98d202

src/Models/Assignor.php
@@ -40,7 +40,7 @@ class Assignor extends Medools\Model
             'id'
         ],
         'address' => [
-            'aryelgois\\Databases\\Models\\Address\\FullAddress',
+            __NAMESPACE__ . '\\FullAddress',
             'id'
         ],
     ];

src/Models/Client.php
@@ -34,7 +34,7 @@ class Client extends Medools\Model
             'id'
         ],
         'address' => [
-            'aryelgois\\Databases\\Models\\Address\\FullAddress',
+            __NAMESPACE__ . '\\FullAddress',
             'id'
         ],
         'assignor' => [