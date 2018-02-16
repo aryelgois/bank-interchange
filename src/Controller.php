aryelgois/bank-interchange

commit 8841e3cd0721b6279fcb6968bb1f15c5e8560059

Update createShippingFile()

src/Controller.php
@@ -20,14 +20,17 @@ abstract class Controller
      * Creates a new ShippingFile Model
      *
      * @param integer[] $title_list A list of Models\Title `id`
-     *                  They all must be for the same assignment.
+     *                              They all must be for the same assignment.
+     * @param string    $cnab       Specify which Cnab to use
      *
      * @return Models\ShippingFile
      *
      * @throws \InvalidArgumentException If $title_list is empty or invalid
      */
-    public static function createShippingFile(array $title_list)
-    {
+    public static function createShippingFile(
+        array $title_list,
+        string $cnab = null
+    ) {
         if (empty($title_list)) {
             throw new \InvalidArgumentException('Title list is empty');
         }
@@ -44,6 +47,7 @@ abstract class Controller
 
         $shipping_file = (new Models\ShippingFile)->fill([
             'assignment' => $assignment,
+            'cnab' => $cnab ?? $assignment->cnab,
             'status' => 0,
         ]);
         $shipping_file->save();

commit f444d04a79912dc47d818b579a280f096b53624a

Fix createShippingFile()
Problem introduced in 8841e3cd0721b6279fcb6968bb1f15c5e8560059

Trying to get property '$assignment->cnab' of non-object

src/Controller.php
@@ -43,7 +43,7 @@ abstract class Controller
         if (count($assignment) != 1) {
             throw new \InvalidArgumentException('Title list is invalid');
         }
-        $assignment = $assignment[0];
+        $assignment = Models\Assignment::getInstance($assignment[0]);
 
         $shipping_file = (new Models\ShippingFile)->fill([
             'assignment' => $assignment,