f51e5a3a99f4ea63fb023daf25660413fb8532e7
Aryel Mota Góis <aryel.gois@gmail.com>
2018-04-24 22:30:17 -0300

    Update FullAddress
    - Empty fields do not cause unnecessary separators
    - If there is a detail, outputLong() places
      the neighborhood in the second line
    - Add detail in outputShort()

--- src/Models/FullAddress.php
+++ src/Models/FullAddress.php
@@ -8,7 +8,6 @@
 namespace aryelgois\BankInterchange\Models;
 
 use aryelgois\Utils\Validation;
-use aryelgois\Medools;
 
 /**
  * This class make it easier to access Address data from Database
@@ -26,15 +25,21 @@ class FullAddress extends \aryelgois\Databases\Models\Address\FullAddress
      */
     public function outputLong()
     {
-        $result = $this->place . ', '
-            . $this->number . ', '
-            . ($this->detail != '' ? $this->detail . ', ' : '')
-            . $this->neighborhood . "\n"
-            . $this->county->name . '/'
-            . $this->county->state->code . ' - '
-            . 'CEP: ' . Validation::cep($this->zipcode);
-
-        return $result;
+        return implode("\n", array_filter([
+            implode(', ', array_filter([
+                $this->place,
+                $this->number,
+                $this->detail,
+                ($this->detail == '' ? $this->neighborhood : ''),
+            ])),
+            implode(', ', array_filter([
+                ($this->detail != '' ? $this->neighborhood : ''),
+                implode(' - CEP: ', array_filter([
+                    $this->county->name . '/' . $this->county->state->code,
+                    Validation::cep($this->zipcode),
+                ])),
+            ])),
+        ]));
     }
 
     /**
@@ -44,13 +49,15 @@ class FullAddress extends \aryelgois\Databases\Models\Address\FullAddress
      */
     public function outputShort()
     {
-        $result = $this->place . ', '
-            . $this->number . ', '
-            . $this->neighborhood . ', '
-            . $this->county->name . '/'
-            . $this->county->state->code . ' '
-            . Validation::cep($this->zipcode);
-
-        return $result;
+        return implode(', ', array_filter([
+            $this->place,
+            $this->number,
+            $this->detail,
+            $this->neighborhood,
+            implode(' ', array_filter([
+                $this->county->name . '/' . $this->county->state->code,
+                Validation::cep($this->zipcode),
+            ]))
+        ]));
     }
 }
