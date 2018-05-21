eb3311729f52f350fb1d0aa0e2331a3a19cb038d
Aryel Mota Góis <aryel.gois@gmail.com>
2018-05-21 18:00:07 -0300

    Remove some id columns in database populate

--- data/bank_interchange_populate.sql
+++ data/bank_interchange_populate.sql
@@ -19,33 +19,33 @@ INSERT INTO `currency_codes` (`currency`, `bank`, `billet`, `cnab240`, `cnab400`
 
 -- Document kinds
 
-INSERT INTO `document_kinds` (`id`, `bank`, `cnab`, `code`, `symbol`, `name`) VALUES
-(1, 2, '240', '02', 'DM', 'Duplicata Mercantil'),
-(2, 2, '240', '04', 'DS', 'Duplicata de Serviço'),
-(3, 2, '240', '08', 'NCC', 'Nota de Crédito Comercial'),
-(4, 2, '240', '09', 'NCE', 'Nota de Crédito a Exportação'),
-(5, 2, '240', '10', 'NCI', 'Nota de Crédito Industrial'),
-(6, 2, '240', '11', 'NCR', 'Nota de Crédito Rural'),
-(7, 2, '240', '12', 'NP', 'Nota Promissória'),
-(8, 2, '240', '17', 'RC', 'Recibo'),
-(9, 2, '240', '20', 'AP', 'Apólice de Seguro'),
-(10, 2, '240', '21', 'ME', 'Mensalidade Escolar'),
-(11, 2, '240', '22', 'PC', 'Parcela de Consórcio'),
-(12, 2, '240', '23', 'NF', 'Nota Fiscal'),
-(13, 2, '240', '99', 'OU', 'Outros'),
-(14, 2, '400', '01', 'DIB', 'Duplicata impressa pelo Banese'),  -- --
-(15, 2, '400', '02', 'NPB', 'NP impressa pelo Banese'),         -- Dummy symbol,
-(16, 2, '400', '05', 'RCB', 'Recibo impresso pelo Banese'),     -- not actually
-(17, 2, '400', '21', 'DIC', 'Duplicata impressa pelo cliente'), -- used by the
-(18, 2, '400', '22', 'NPC', 'NP impressa pelo cliente'),        -- bank.
-(19, 2, '400', '25', 'RIC', 'Recibo impresso pelo cliente'),    -- --
-(20, 3, '400', '01', 'DM', 'Duplicata Mercantil'),
-(21, 3, '400', '02', 'NP', 'Nota Promissória'),
-(22, 3, '400', '03', 'CH', 'Cheque'),
-(23, 3, '400', '04', 'CR', 'Carnê'), -- Dummy symbol
-(24, 3, '400', '05', 'RC', 'Recibo'),
-(25, 3, '400', '06', 'DS', 'Duplicata Prest. Serviços'),
-(26, 3, '400', '19', 'OU', 'Outros');
+INSERT INTO `document_kinds` (`bank`, `cnab`, `code`, `symbol`, `name`) VALUES
+(2, '240', '02', 'DM', 'Duplicata Mercantil'),
+(2, '240', '04', 'DS', 'Duplicata de Serviço'),
+(2, '240', '08', 'NCC', 'Nota de Crédito Comercial'),
+(2, '240', '09', 'NCE', 'Nota de Crédito a Exportação'),
+(2, '240', '10', 'NCI', 'Nota de Crédito Industrial'),
+(2, '240', '11', 'NCR', 'Nota de Crédito Rural'),
+(2, '240', '12', 'NP', 'Nota Promissória'),
+(2, '240', '17', 'RC', 'Recibo'),
+(2, '240', '20', 'AP', 'Apólice de Seguro'),
+(2, '240', '21', 'ME', 'Mensalidade Escolar'),
+(2, '240', '22', 'PC', 'Parcela de Consórcio'),
+(2, '240', '23', 'NF', 'Nota Fiscal'),
+(2, '240', '99', 'OU', 'Outros'),
+(2, '400', '01', 'DIB', 'Duplicata impressa pelo Banese'),  -- --
+(2, '400', '02', 'NPB', 'NP impressa pelo Banese'),         -- Dummy symbol,
+(2, '400', '05', 'RCB', 'Recibo impresso pelo Banese'),     -- not actually
+(2, '400', '21', 'DIC', 'Duplicata impressa pelo cliente'), -- used by the
+(2, '400', '22', 'NPC', 'NP impressa pelo cliente'),        -- bank.
+(2, '400', '25', 'RIC', 'Recibo impresso pelo cliente'),    -- --
+(3, '400', '01', 'DM', 'Duplicata Mercantil'),
+(3, '400', '02', 'NP', 'Nota Promissória'),
+(3, '400', '03', 'CH', 'Cheque'),
+(3, '400', '04', 'CR', 'Carnê'), -- Dummy symbol
+(3, '400', '05', 'RC', 'Recibo'),
+(3, '400', '06', 'DS', 'Duplicata Prest. Serviços'),
+(3, '400', '19', 'OU', 'Outros');
 
 -- Shipping File Movements
 -- TODO check if missing for cnab 400
@@ -88,13 +88,13 @@ INSERT INTO `shipping_file_movements` (`bank`, `cnab`, `code`, `name`) VALUES
 -- Wallets
 -- TODO verify data
 
-INSERT INTO `wallets` (`id`, `bank`, `cnab`, `code`, `operation`, `symbol`, `name`) VALUES
-(1, 1, '240', '1', '?', 'CR', 'Cobrança Registrada'),
-(2, 2, '240', '1', '?', 'CS', 'Cobrança Simples'),
-(3, 2, '400', '2', '?', 'CS', 'Cobrança Simples'),
-(4, 2, '400', '7', '?', 'CE', 'Cobrança Expressa'),
-(5, 3, '400', '1', '21', 'CS', 'Cobrança Simples Escritural - Boleto Emitido Pelo Banco'),
-(6, 3, '400', '2', '41', 'CV', 'Cobrança Vinculada – Boleto Emitido Pelo Banco'),
-(7, 3, '400', '4', '21', 'CS', 'Cobrança Simples - Boleto Emitido Pelo Cliente'),
-(8, 3, '400', '5', '41', 'CV', 'Cobrança Vinculada - Boleto Emitido Pelo Cliente'),
-(9, 3, '400', 'I', '51', 'SR', 'Cobrança Simplificada (Sem Registro)');
+INSERT INTO `wallets` (`bank`, `cnab`, `code`, `operation`, `symbol`, `name`) VALUES
+(1, '240', '1', '?', 'CR', 'Cobrança Registrada'),
+(2, '240', '1', '?', 'CS', 'Cobrança Simples'),
+(2, '400', '2', '?', 'CS', 'Cobrança Simples'),
+(2, '400', '7', '?', 'CE', 'Cobrança Expressa'),
+(3, '400', '1', '21', 'CS', 'Cobrança Simples Escritural - Boleto Emitido Pelo Banco'),
+(3, '400', '2', '41', 'CV', 'Cobrança Vinculada – Boleto Emitido Pelo Banco'),
+(3, '400', '4', '21', 'CS', 'Cobrança Simples - Boleto Emitido Pelo Cliente'),
+(3, '400', '5', '41', 'CV', 'Cobrança Vinculada - Boleto Emitido Pelo Cliente'),
+(3, '400', 'I', '51', 'SR', 'Cobrança Simplificada (Sem Registro)');
