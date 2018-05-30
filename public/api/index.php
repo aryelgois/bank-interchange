571e9202dcef0728aa2feb7f9030b4c739563fa0
Aryel Mota Góis <aryel.gois@gmail.com>
2018-05-29 21:26:16 -0300

    Load BankBillet data

--- public/api/index.php
+++ public/api/index.php
@@ -3,7 +3,13 @@
 require_once __DIR__ . '/../bootstrap.php';
 
 use aryelgois\MedoolsRouter;
+use aryelgois\BankInterchange;
 use Symfony\Component\Yaml\Yaml;
+
+BankInterchange\BankBillet\Controller::loadData(
+    Yaml::parseFile(APP_ROOT . '/config/billet.yml'),
+    APP_ROOT . '/assets/logos'
+);
 
 $request = from_globals();
 
